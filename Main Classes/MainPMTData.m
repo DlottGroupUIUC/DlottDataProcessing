7ce4922ba926e444ad8fb4159aad51774f75fbeb
shiny-metal-class <31256512+shiny-metal-class@users.noreply.github.com>
2019-08-19 18:58:44 -0500

    setting up PMT

--- Main Classes/MainPMTData.m
+++ Main Classes/MainPMTData.m
@@ -9,7 +9,21 @@ classdef MainPMTData < handles
             obj.mainFig = mainFig;
             obj.handles = guidata(obj.mainFig);
             obj.DataStorage = cell(T,1);
-            obj.fileNames = obj.handles
+            obj.fileNames = obj.handles;
+        end
+    end
+    methods(Access = private)
+        function [binTime,binRad] = TDMS_Shorten()
+            
+        end
+        function [EmissionFilterLibrary,CalFilterLibrary] = ReadFilterData(txt)
+            fileID = fopen(sprintf(obj.fName));
+            textscan(fileID,'%s',34);
+            Offset = textscan(fileID,'%f',1);
+            Offset = double(Offset{1});
+            textscan(fileID,'%s',7);
+            List = textscan(fileID,'%f,%f,%f',1);
+            fclose(fileID);
         end
     end
     
